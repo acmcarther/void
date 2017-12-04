6b791ae4b510d4da94d2fb772a8f932abfce8aec
Alex McArther <acmcarther@google.com>
2017-12-04 09:18:01 -0800

    Start a window in the client (make sure to exit terminal multiplexers)

--- client/client.rs
+++ client/client.rs
@@ -1,7 +1,40 @@
 #[macro_use]
 extern crate log;
+#[macro_use]
 extern crate ash;
+extern crate winit;
+#[macro_use]
+extern crate lazy_static;
+
+struct Sys {
+  pub events_loop: winit::EventsLoop,
+  pub window: winit::Window,
+}
 
 pub fn run() {
-  debug!("Starting Client");
+  let sys = init();
+  start_main_loop(sys);
+}
+
+/** Initializes globals for main loop. */
+fn init() -> Sys {
+  let events_loop = winit::EventsLoop::new();
+  let window = winit::WindowBuilder::new()
+              .with_title("Void")
+              .with_dimensions(1920, 1080)
+              .build(&events_loop)
+              .unwrap();
+  Sys {
+    events_loop: events_loop,
+    window: window
+  }
+}
+
+/** Runs the game application in an infinite loop. */
+fn start_main_loop(sys: Sys) {
+  let b = true;
+
+  while b {
+    std::thread::sleep(std::time::Duration::new(1, 0))
+  }
 }
