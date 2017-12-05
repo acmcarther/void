413aa97174874890eddcf3f87133bf17d0d174f1
Alex McArther <acmcarther@google.com>
2017-12-04 18:51:50 -0800

    Move server into src

---
+++ server/src/lib.rs
@@ -0,0 +1,14 @@
+/** A game backend installation in a server cluster. */
+struct ServerNode;
+
+impl ServerNode {
+  fn run_forever(self) {
+  }
+}
+
+
+pub fn run() {
+  let node = ServerNode;
+
+  node.run_forever()
+}
