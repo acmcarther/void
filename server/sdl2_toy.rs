f4d0cbbc0561b3ede0d9a997179a07565935b8b6
Alex McArther <acmcarther@google.com>
2018-01-16 18:21:40 -0800

    Wip vulkan 1

--- server/sdl2_toy.rs
+++ server/sdl2_toy.rs
@@ -1,8 +1,13 @@
 extern crate sdl2;
+extern crate vk_sys as vk;
+
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
 use std::time::Duration;
+use std::ffi::CString;
+use std::ptr;
 
 fn main() {
   println!("1");
@@ -14,10 +19,15 @@ fn main() {
   let video_subsystem = sdl_context.video().unwrap();
   println!("3");
 
+  vulkan();
+
   let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
       .position_centered()
+      .vulkan()
       .build()
       .unwrap();
+
+  println!("3");
 
   let mut canvas = window.into_canvas().build().unwrap();
 
@@ -44,4 +54,27 @@ fn main() {
       canvas.present();
       ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
+}
+
+fn vulkan() {
+  let vk_application_info = vk::ApplicationInfo {
+    sType: vk::STRUCTURE_TYPE_APPLICATION_INFO,
+    pNext: ptr::null(),
+    pApplicationName: CString::new("sdl2_toy").unwrap().as_ptr(),
+    applicationVersion: 1,
+    pEngineName: CString::new("No Engine").unwrap().as_ptr(),
+    engineVersion: 1,
+    apiVersion: 0
+  };
+
+  let vk_instance_create_info = vk::InstanceCreateInfo {
+    sType: vk::STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    pApplicationInfo: &vk_application_info as *const _,
+    flags: 0,
+    pNext: ptr::null(),
+    enabledLayerCount: 0,
+    ppEnabledLayerNames: ptr::null(),
+    enabledExtensionCount: 0,
+    ppEnabledExtensionNames: ptr::null(),
+  };
 }
